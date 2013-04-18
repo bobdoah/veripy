647890413ae88e5233493bcf95f9b3d4257a0a81
Robert Williams <robert.williams@hds.com>
2013-04-18 13:49:22 +0100

    Fixed the Resolution Wait Queue, Single Queue test
    Modified the test so that it only sends a single neighbor advertisment,
    in response to the neighbor solicitations received. Updated the test to
    only ensure at least one Echo Reply is received, as RFC2460 only
    mandates a queue depth of one (and that's what the TAHI suite checks
    for)

--- contrib/rfc4861/resolution_wait_queue.py
+++ contrib/rfc4861/resolution_wait_queue.py
@@ -1,3 +1,5 @@
+import itertools
+
 from scapy.all import *
 from veripy.assertions import *
 from veripy.models import ComplianceTestCase
@@ -18,36 +20,34 @@ class ResolutionWaitQueueSingleQueueTestCase(ComplianceTestCase):
     restart_uut = True
 
     def run(self):
-        seq = []
-        for i in range(0, 3):
-            self.logger.info("Sending ICMP Echo Request %d (seq: %d)..." % (i, self.next_seq()))
-            seq.append(self.seq())
-            
-            self.node(1).send(
-                IPv6(src=str(self.node(1).link_local_ip()), dst=str(self.target(1).link_local_ip()))/
-                    ICMPv6EchoRequest(seq=self.seq()))
+        seq = [self.next_seq() for _ in itertools.repeat(None, 3)]
+        self.logger.info("Sending three ICMP Echo Requests (seq numbers: %s)..." % ", ".join((str(s) for s in seq)))
+
+        self.node(1).send(IPv6(src=str(self.node(1).link_local_ip()), dst=str(self.target(1).link_local_ip()))/
+                    ICMPv6EchoRequest(seq=seq))
 
         self.logger.info("Checking for Neighbor Solicitations...")
         r1 = self.node(1).received(src=self.target(1).link_local_ip(), dst=self.node(1).link_local_ip().solicited_node(), type=ICMPv6ND_NS)
-        assertGreaterThan(0, len(r1), "expected to receive at least 1 ICMPv6 Neighbor Solicitation")
-
+        assertGreaterThan(0, len(r1), "expected to receive at least one ICMPv6 Neighbor Solicitation")
         for p in r1:
             assertEqual(self.node(1).link_local_ip(), p.getlayer(ICMPv6ND_NS).tgt, "expected the Neighbor Solicitation to be for TN1's link local address")
+
             
-            self.logger.info("Sending Neighbor Advertisement in response to Neighbor Solicitation...")
-            self.node(1).send(
-                IPv6(src=str(self.node(1).link_local_ip()), dst=str(self.target(1).link_local_ip()))/
-                    ICMPv6ND_NA(R=0, S=1, O=1, tgt=str(self.node(1).link_local_ip()))/
-                        ICMPv6NDOptDstLLAddr(lladdr=self.node(1).iface(0).ll_addr))
+        self.logger.info("Sending Neighbor Advertisement in response to Neighbor Solicitation...")
+        self.node(1).send(
+            IPv6(src=str(self.node(1).link_local_ip()), dst=str(self.target(1).link_local_ip()))/
+                ICMPv6ND_NA(R=0, S=1, O=1, tgt=str(self.node(1).link_local_ip()))/
+                    ICMPv6NDOptDstLLAddr(lladdr=self.node(1).iface(0).ll_addr))
             
         self.ui.wait(5)
         self.logger.info("Checking for ICMPv6 Echo Replies...")
         r2 = self.node(1).received(src=self.target(1).link_local_ip(), type=ICMPv6EchoReply)
 
-        assertEqual(3, len(r2), "expected to receive 3 ICMPv6 Echo Replies")
+        assertGreaterThan(0, len(r2), "expected to receive at least one ICMPv6 Echo Replies")
 
         for p in r2:
-            assertTrue(p.getlayer(ICMPv6EchoReply).seq in seq, "expected an ICMPv6 echo reply for each request sent")
+            assertTrue(p.getlayer(ICMPv6EchoReply).seq in seq, "expected an ICMPv6 echo reply with sequence number in: %s, got %s" % (
+                ", ".join(str(s) for s in seq), p.getlayer(ICMPv6EchoReply).seq))
 
 
 class ResolutionWaitQueueMultipleQueueTestCase(ComplianceTestCase):
