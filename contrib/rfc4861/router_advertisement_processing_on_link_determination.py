a90cbb92cf4bf31d9fca1eee04cbd28b8b9ab09f
Robert Williams <robert.williams@hds.com>
2013-08-01 18:55:19 +0100

    Fixed the RA on-link determination test

--- contrib/rfc4861/router_advertisement_processing_on_link_determination.py
+++ contrib/rfc4861/router_advertisement_processing_on_link_determination.py
@@ -26,21 +26,35 @@ class OnLinkDeterminationTestCase(ComplianceTestCase):
         self.router(1).send(
             IPv6(src=str(self.router(1).link_local_ip(iface=1)), dst="ff02::1")/
                 ICMPv6ND_RA(prf=1)/
-                    ICMPv6NDOptPrefixInfo(prefixlen=96, prefix=self.link(2).v6_prefix, L=True)/
+                    ICMPv6NDOptPrefixInfo(prefixlen=96, prefix=str(self.node(2).global_ip()), L=True)/
                     ICMPv6NDOptSrcLLAddr(lladdr=self.router(1).iface(1).ll_addr), iface=1)
 
         self.logger.info("Sending an Echo Request from TN1...")
         self.node(1).send(
-            IPv6(src=str(self.node(1).global_ip()), dst=str(self.target(1).global_ip()))/
+            IPv6(src=str(self.node(2).global_ip()), dst=str(self.target(1).global_ip()))/
                 ICMPv6EchoRequest(seq=self.next_seq()))
 
+        self.logger.info("Checking for a Neighbor Solicitation")
+        r1 = self.node(1).received(dst=self.node(2).global_ip().solicited_node(), src=self.target(1).link_local_ip(), type=ICMPv6ND_NS)
+        assertGreaterThanOrEqualTo(1, len(r1), "expected the NUT to send a Neighbor Solicitation to the on-link address")
+        assertEqual(self.node(2).global_ip(), r1[0][ICMPv6ND_NS].tgt, "expected the target of the neighbor solicitation"
+            " to be %s, but it was %s" % (self.node(2).global_ip(), r1[0][ICMPv6ND_NS].tgt)
+        )
+
+        self.logger.info("Sending a Neighbor Advertisement")
+        self.node(1).send(
+            IPv6(src=str(self.node(2).global_ip()), dst=r1[0][IPv6].src)/
+                ICMPv6ND_NA(tgt=str(self.node(2).global_ip()), R=False, S=True, O=True)/
+                    ICMPv6NDOptDstLLAddr(lladdr=self.node(1).iface(0).ll_addr))
+
+
         self.logger.info("Checking for an ICMPv6 Echo Reply...")
-        r3 = self.node(1).received(src=self.target(1).global_ip(), seq=self.seq(), type=ICMPv6EchoReply)
-        assertEqual(1, len(r3), "expected to receive an ICMPv6 Echo Reply (seq: %d)" % (self.seq()))
+        r1 = self.node(1).received(src=self.target(1).global_ip(), dst=self.node(2).global_ip(), seq=self.seq(), type=ICMPv6EchoReply, raw=True)
+        assertEqual(1, len(r1), "expected to receive an ICMPv6 Echo Reply (seq: %d)" % (self.seq()))
 
         self.logger.info("Grabbing the Echo Reply to see if TR1 forwarded it...")
         r2 = self.router(1).received(iface=1, src=self.target(1).global_ip(), dst=self.node(2).global_ip(), seq=self.seq(), type=ICMPv6EchoReply, raw=True)
 
-        assertEqual(self.node(2).global_ip(), r2[0][IPv6].dst, "expected the ICMPv6 Echo Reply dst to be TN2's global address")
-        assertEqual(self.target(1).ll_addr(), r2[0][Ether].src, "expected the ICMPv6 Echo Reply Ethernet src to be the UUT")
-        assertNotEqual(self.router(1).iface(1).ll_addr, r2[0][Ether].dst, "did not expect the ICMPv6 Echo Reply to be sent through TR1")
+        assertEqual(self.node(2).global_ip(), r1[0][IPv6].dst, "expected the ICMPv6 Echo Reply dst to be TN2's global address")
+        assertEqual(self.target(1).ll_addr(), r1[0][Ether].src, "expected the ICMPv6 Echo Reply Ethernet src to be the UUT")
+        assertNotEqual(self.router(1).iface(1).ll_addr, r1[0][Ether].dst, "did not expect the ICMPv6 Echo Reply to be sent through TR1")
